8da9c0a3d1cb8c98c484e142b1c542996b00e572
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-10-25 04:27:30 +0200

    tests: refactor

--- flask_gbapp/test_app.py
+++ flask_gbapp/test_app.py
@@ -1,75 +1,110 @@
 import unittest
+from unittest.mock import patch
 
 from scripts.parser import Parser
 from scripts.api_googlemaps import ApiGoogleMap
 from scripts.api_wikipedia import ApiWikipedia
-from unittest.mock import patch
 
 
-class TestParser(unittest.TestCase):
+class TestApp(unittest.TestCase):
+    """ Tests the Functionalities """
 
-	def test_parser(self):
-		parser = Parser()
+    def setUp(self):
+        """ Set attributs """
+        self.parser = Parser()
+        self.api_googlemaps = ApiGoogleMap()
+        self.api_wikipedia = ApiWikipedia()
 
-		question = "Indique moi ou est le musée du louvre!"
+    def test_parser(self):
+        """ Test the parser """
 
-		result = parser.extract(question)
+        question = "Sais-tu où est le musée du louvre ?"
+        result = self.parser.extract(question)
 
-		self.assertEqual(result, "musée louvre ")
+        self.assertEqual(result, "musée louvre")
 
-	# QUand tu vois requests.get dans api_googlemaps tu le remplaces par mock_json (qui est l'attribut)
-	@patch("scripts.api_googlemaps.requests.get")
-	def test_api_gmaps(self, mock_json):
-		mock_json.return_value.json.return_value = {'results': [{'address_components': [{'long_name': 'Place Jacques Rueff', 'short_name': 'Place Jacques Rueff', 'types': ['route']}, {'long_name': 'Paris', 'short_name': 'Paris', 'types': ['locality', 'political']}, {'long_name': 'Arrondissement de Paris', 'short_name': 'Arrondissement de Paris', 'types': ['administrative_area_level_2', 'political']}, {'long_name': 'Île-de-France', 'short_name': 'Île-de-France', 'types': ['administrative_area_level_1', 'political']}, {'long_name': 'France', 'short_name': 'FR', 'types': ['country', 'political']}, {'long_name': '75007', 'short_name': '75007', 'types': ['postal_code']}], 'formatted_address': 'CONNAID Jacques Rueff, 75007 Paris, France', 'geometry': {'location': {'lat': 48.858257, 'lng': 2.2946056}, 'location_type': 'GEOMETRIC_CENTER', 'viewport': {'northeast': {'lat': 48.85960598029151, 'lng': 2.295954580291502}, 'southwest': {'lat': 48.85690801970851, 'lng': 2.293256619708498}}}, 'place_id': 'ChIJ____vx9w5kcRg3gQZBz71Ag', 'plus_code': {'compound_code': 'V75V+8R Paris, France', 'global_code': '8FW4V75V+8R'}, 'types': ['establishment', 'museum', 'point_of_interest']}], 'status': 'OK'}
+    @patch("scripts.api_googlemaps.requests.get")
+    def test_api_gmaps_ok(self, mock_json):
+        """ Test Google Maps (API) """
 
-		data = mock_json.return_value.json.return_value["results"]
+        mock_json.return_value.json.return_value = {
+            'results': [{'address_components': [{
+                'long_name': 'Place Jacques Rueff',
+                'short_name': 'Place Jacques Rueff', 'types': ['route']},
+                {'long_name': 'Paris', 'short_name': 'Paris',
+                 'types': ['locality', 'political']},
+                {'long_name': 'Arrondissement de Paris',
+                 'short_name': 'Arrondissement de Paris',
+                 'types': ['administrative_area_level_2', 'political']},
+                {'long_name': 'Île-de-France', 'short_name': 'Île-de-France',
+                 'types': ['administrative_area_level_1', 'political']},
+                {'long_name': 'France', 'short_name': 'FR',
+                 'types': ['country', 'political']},
+                {'long_name': '75007', 'short_name': '75007',
+                 'types': ['postal_code']}],
+                 'formatted_address':
+                 'Place Jacques Rueff, 75007 Paris, France',
+                 'geometry': {'location': {'lat': 48.858257, 'lng': 2.2946056},
+                              'location_type': 'GEOMETRIC_CENTER',
+                              'viewport': {'northeast': {
+                                           'lat': 48.85960598029151,
+                                           'lng': 2.295954580291502},
+                                           'southwest': {
+                                           'lat': 48.85690801970851,
+                                           'lng': 2.293256619708498}}},
+                 'place_id': 'ChIJ____vx9w5kcRg3gQZBz71Ag',
+                 'plus_code': {'compound_code': 'V75V+8R Paris, France',
+                               'global_code': '8FW4V75V+8R'},
+                 'types': ['establishment', 'museum', 'point_of_interest']}],
+            'status': 'OK'
+        }
 
-		api_googlemaps = ApiGoogleMap()
-		data_googlemaps = api_googlemaps.find("adresse tour mo")
+        data = mock_json.return_value.json.return_value["results"]
+        result = {
+            "formatted_address": data[0]['formatted_address'],
+            "route": data[0]['address_components'][0]["long_name"],
+            "locality": data[0]['address_components'][1]["long_name"],
+            "lat": data[0]['geometry']["location"]["lat"],
+            "lng": data[0]['geometry']["location"]["lng"]
+        }
 
-		r = {
-			"formatted_address": data[0]['formatted_address'],
-			"route": data[0]['address_components'][0]["long_name"] + " " + data[0]['address_components'][1]["long_name"],
-			"lat": data[0]['geometry']["location"]["lat"],
-			"lng": data[0]['geometry']["location"]["lng"]
-		}
-		print("RR : ", api_googlemaps.find("adresse tour mo"), r)
+        self.assertEqual(self.api_googlemaps.find("tour eiffel"), result)
 
-		self.assertEqual(api_googlemaps.find("adresse tour mo"), r)
-		
-	@patch("scripts.api_wikipedia.wikipedia.search")
-	@patch("scripts.api_wikipedia.wikipedia.summary")
-	@patch("scripts.api_wikipedia.wikipedia.page")
-	def test_api_wiki(self, mock_search, mock_summary, mock_page):
-		"""
-		#mock_search.return_value = "Eiffel"
-		print("m :", mock_page) # search
-		print("m :", mock_search) # page
-		print("m :", mock_summary) #summary"""
-		
-		api_wikipedia = ApiWikipedia()
-			
+    @patch("scripts.api_googlemaps.requests.get")
+    def test_api_gmaps_error(self, mock_json):
+        """ Test the error of Google Maps (API) """
 
-		mock_page.return_value = "Eiffel"
-		mock_search().url = "http://"
-		mock_summary.return_value = "La tour eiffel blablabla"
+        mock_json.return_value.json.side_effect = IndexError
+        result = self.api_googlemaps.find("adresse tour eiffel")
 
-		"""
-		print("m :", mock_search)
-		print("m :", mock_summary)
-		print("m :", mock_page)"""
+        self.assertFalse(result)
 
+    @patch("scripts.api_wikipedia.wikipedia.search")
+    @patch("scripts.api_wikipedia.wikipedia.summary")
+    @patch("scripts.api_wikipedia.wikipedia.page")
+    def test_api_wiki_ok(self, mock_page, mock_summary, mock_search):
+        """ Test Wikipedia (API) """
 
-		
+        mock_search.return_value = "Eiffel"
+        mock_page().url = "https://"
+        mock_summary.return_value = "La tour eiffel se trouve..."
 
-		r = {
-			"summary": mock_summary.return_value,
-			"url": mock_search().url
-		}
-		print("DD", api_wikipedia.find("Eiffel"), r)	
+        result = {
+            "summary": mock_summary.return_value,
+            "url": mock_page().url
+        }
 
-		self.assertEqual(api_wikipedia.find("Eiffel"), r)
-		
+        self.assertEqual(self.api_wikipedia.find("Eiffel"), result)
+
+    @patch("scripts.api_wikipedia.wikipedia.search")
+    def test_api_wiki_error(self, mock_search):
+        """ Test the error of Wikipedia (API) """
+
+        mock_search.side_effect = IndexError
+        result = self.api_wikipedia.find("Eiffel")
+
+        self.assertFalse(result)
+
 
 if __name__ == '__main__':
     unittest.main()
