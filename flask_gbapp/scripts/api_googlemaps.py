bd938a7fef266f1e414c490e2881daec3ef0c982
jeremy10000 <jeremyjulienvisio@outlook.fr>
2019-10-25 04:36:49 +0200

    gmaps refactor

--- flask_gbapp/scripts/api_googlemaps.py
+++ flask_gbapp/scripts/api_googlemaps.py
@@ -1,53 +1,41 @@
 import requests
 
+
 class ApiGoogleMap:
+    """ Request for information about a place. """
 
-	def __init__(self):
-		self.url = "https://maps.googleapis.com/maps/api/geocode/json?"
+    def __init__(self):
+        """ Geocoding maps API """
+        self.url = "https://maps.googleapis.com/maps/api/geocode/json?"
+        self.map = dict()
 
-	def find(self, keywords):
-		params = {
-        	'address': keywords,
-        	'key': ''
-    	}
-		api_request = requests.get(url=self.url, params=params)
-		response = api_request.json()
-		data = response["results"]
+    def find(self, keywords):
+        """ Creation of a dictionary to keep the information found. """
 
-		route = str()
-		point_of_interest = None
-		locality = str()
+        params = {
+            'address': keywords,
+            'key': 'API_KEY'
+        }
 
-		# dico (key) for : list comprehension
-		try:
-			for component in data[0]["address_components"]:
-				if 'route' in component['types']:
-					#print(" ### route : ", component['long_name'])
-					route = component['long_name']
+        try:
+            api_request = requests.get(url=self.url, params=params)
+            data = api_request.json()["results"]
 
-				if 'point_of_interest' in component['types']:
-					point_of_interest = component['long_name']
+            # example : eiffel tower
+            # {'establishment': 'Champ de Mars',
+            # 'route': 'Avenue Anatole France', 'locality': 'Paris'}
+            self.map = {
+                k["types"][0]: k["long_name"]
+                for k in data[0]["address_components"]
+                if "route" in k["types"] or
+                   "establishment" in k["types"] or
+                   "locality" in k["types"]
+            }
+            self.map["lat"] = data[0]['geometry']['location']['lat']
+            self.map["lng"] = data[0]['geometry']['location']['lng']
+            self.map["formatted_address"] = data[0]['formatted_address']
 
-				if 'locality' in component['types']:
-					#print(" ### locality : ", component['long_name'])
-					locality = component['long_name']
+        except IndexError:
+            return False
 
-			formatted_address = data[0]['formatted_address']
-			#print("*********** : formatted_address : ", formatted_address)
-			lat = data[0]['geometry']['location']['lat']
-			lng = data[0]['geometry']['location']['lng']
-		except IndexError:
-			return False
-
-		if point_of_interest is not None:
-			print("### : route = ", route)
-			route = point_of_interest
-			print("### : route (changement?) = ", route)
-
-
-		return {
-			"formatted_address": formatted_address,
-			"route": route + " " + locality,
-			"lat": lat,
-			"lng": lng
-		}
+        return self.map
